b23a4c6ea6a37e6994faa86777ae6bdcefe87cb9
MatteoLacki <matteo.lacki@gmail.com>
2019-10-04 14:17:46 +0200

    Modified the pipeline to use .xml input for iaDBs.

--- proteome_tools_data/rerun_iadbs.py
+++ proteome_tools_data/rerun_iadbs.py
@@ -4,11 +4,14 @@ from furious_fastas.fastas import Fastas
 from pathlib import Path
 from vodkas import iadbs
 from vodkas.logging import get_logger
+import logging
+from shutil import copyfile
+import json
 
 from proteome_tools_data.file_iteration import all_res
 
 proj_folder = Path(r"//MSSERVER/restoredData/proteome_tools")
-pool2 		= Path("D:/projects/proteome_tools/RES/pool2")
+pool2       = Path("D:/projects/proteome_tools/RES/pool2")
 parameters_file = proj_folder/"params/515.xml"
 
 logging.basicConfig(filename=pool2/"pool2.log",
@@ -16,36 +19,67 @@ logging.basicConfig(filename=pool2/"pool2.log",
                     level=logging.INFO)
 logger = get_logger(__name__)
 
-def to_ncbi(F):
-    for f in F:
-        if not 'REVERSE' in f.header:
-            acc, desc = f.header.split('|')
-            acc = acc[1:]
-            yield NCBIgeneralFasta(acc, acc, desc, f.sequence)
+# def to_ncbi(F):
+#     for f in F:
+#         if not 'REVERSE' in f.header:
+#             acc, desc = f.header.split('|')
+#             acc = acc[1:]
+#             yield NCBIgeneralFasta(acc, acc, desc, f.sequence)
 
-def reverse_fasta(fasta_file):
-    F = Fastas(to_ncbi(fastas(fasta_file)))
-    F.reverse()
-    out_path = fasta_file.parent/f"{fasta_file.stem}_reversed.fasta"
-    F.write(out_path)
-    return out_path
+# def reverse_fasta(fasta_file):
+#     F = Fastas(to_ncbi(fastas(fasta_file)))
+#     F.reverse()
+#     out_path = fasta_file.parent/f"{fasta_file.stem}_reversed.fasta"
+#     F.write(out_path)
+#     return out_path
 
-problems = []
-for p in all_res:
-    fasta_file = next(f for f in p.glob('*.fasta') if not '_reversed' in str(f))
-    if not fasta_file:
-        print(f'Problem in {p}')
-        problems.append(p)
-    else:
-        fasta_rev = reverse_fasta(fasta_file)
+# problems = []
+# for p in all_res:
+#     fasta_file = next(f for f in p.glob('*.fasta') if not '_reversed' in str(f))
+#     if not fasta_file:
+#         print(f'Problem in {p}')
+#         problems.append(p)
+#     else:
+#         fasta_rev = reverse_fasta(fasta_file)
+
+# from pprint import pprint
+
+# p = all_res[0]
+# res_folders = {p.stem:p  for p in all_res}
+# p = res_folders['T170722_109']
+
+# fasta_file = next(f for f in p.glob('*.fasta') if '_reversed' in str(f))
+# output_dir = p/'reversed_search'
+# output_dir.mkdir(exist_ok=True)
+# cmd = iadbs(input_file=p/f"{p.stem}_Pep3D_Spectrum.bin",
+#                    output_dir=output_dir,
+#                    fasta_file=fasta_file,
+#                    parameters_file=parameters_file)
+# pprint(cmd)
+
+#  C:\SYMPHONY_VODKAS\plgs\iaDBs.exe -paraXMLFileName \\MSSERVER\restoredData\proteome_tools\params\515.xml -pep3DFileName D:\projects\proteome_tools\RES\pool1\T1707\T170722_109\T170722_109_Pep3D_Spectrum.bin -proteinFASTAFileName D:\projects\proteome_tools\RES\pool1\T1707\T170722_109\051_reversed.fasta -outputDirName D:\projects\proteome_tools\RES\pool1\T1707\T170722_109\reversed_search -WriteXML 1 -WriteBinary 1 -bDeveloperCSVOutput 0
+# from subprocess import Popen, TimeoutExpired, run, PIPE
+
+# pr = Popen(cmd, stdout=PIPE)
+# out, err = pr.communicate(timeout=60)
+
 
 problems2 = []
 for p in all_res:
-	fasta_file = next(f for f in p.glob('*.fasta') if '_reversed' in str(f))
-    if fasta_file:
-    	iadbs_status = iadbs(input_file=p/f"{p.stem}_Pep3D_Spectrum",
-    		  				 output_file=p/f"{p.stem}_IA_workflow_reversed.xml",
-    		  				 fasta_file,
-    		  				 parameters_file)
-    else:
-        problems2.append(p)
+    try:
+        fasta_file = next(f for f in p.glob('*.fasta') if '_reversed' in str(f))
+        output_dir = p/'reversed_search'
+        output_dir.mkdir(exist_ok=True)
+        res_path,_ = iadbs(input_file=p/f"{p.stem}_Pep3D_Spectrum.xml",
+                           output_dir=output_dir,
+                           fasta_file=fasta_file,
+                           parameters_file=parameters_file)
+        respath = res_path.with_suffix('.xml')
+        final_loc = respath.parent.parent/f"{respath.stem}_reversed.xml"
+        copyfile(str(respath), str(final_loc))
+    except Exception as e:
+        problems2.append((p,repr(e)))
+
+if problems2:
+    with open("D:/projects/proteome_tools/RES/pool2.probs") as h:
+        json.dump(problems2, h, indent=4)
