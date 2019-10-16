af8385687607ad9d90cc2225452d94b04c9cec6c
MatteoLacki <matteo.lacki@gmail.com>
2019-10-16 14:02:38 +0200

    Remaking all the reports.

--- proteome_tools_data/fix_wrong_search1.py
+++ proteome_tools_data/fix_wrong_search1.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 from proteome_tools_data.recalculate_coverages import iter_res
 from proteome_tools_data.file_iteration import all_res, get_pools_proj2fasta
+from proteome_tools_data.coverages.coverages import get_input_for_coverages, get_coverages
 
 pt = Path("J:/proteome_tools")
 
@@ -23,7 +24,6 @@ logging.basicConfig(filename=res_path.parent/"fix_pool1_fastas.log",
                     format='FIXING_FASTAS %(asctime)s:%(name)s:%(levelname)s:%(message)s:',
                     level=logging.INFO)
 logger = get_logger(__name__)
-
 
 def reformulate_fasta(f):
     pack, desc = f.header.split('|')
@@ -51,19 +51,51 @@ for raw, fasta in pool1fix:
         problems.append((str(f), repr(e)))
         logger.warning(repr(e))
 
+# troubles = []
+# # raw,_ = pool1fix[0]
+# for raw,_ in pool1fix:
+#     raw = Path(raw)
+#     res_folder = res_path/raw.parent.stem/raw.stem
+#     try:
+#         df,_ = wx2csv(res_folder/'reversed_search'/f"{raw.stem}_IA_workflow.xml",
+#                       res_folder/'reversed_search'/f"{raw.stem}_report.csv")
+#     except Exception as e:
+#         print(e)
+#         troubles.append(e)
+
+res = Path(r"D:/projects/proteome_tools/RES")
+
+# RECALCULATING ALL THE BLOODY REPORTS
 troubles = []
-# raw,_ = pool1fix[0]
-for raw,_ in pool1fix:
-    raw = Path(raw)
-    res_folder = res_path/raw.parent.stem/raw.stem
+# proj_folder = next(res.glob("pool*/*/*"))
+for proj_folder in res.glob("pool*/*/*"):
     try:
-        df,_ = wx2csv(res_folder/'reversed_search'/f"{raw.stem}_IA_workflow.xml",
-                      res_folder/'reversed_search'/f"{raw.stem}_report.csv")
+        df,_ = wx2csv(proj_folder/'reversed_search'/f"{proj_folder.stem}_IA_workflow.xml",
+                      proj_folder/'reversed_search'/f"{proj_folder.stem}_report.csv")
     except Exception as e:
         print(e)
         troubles.append(e)
 
-RES = pd.DataFrame(iter_res())
+# proj_folder = next(res.glob("pool*/*/*"))
+def iter_coverages(res):
+    for proj_folder in res.glob("pool*/*/*"):
+        report_path = proj_folder/"reversed_search"/f"{proj_folder.stem}_report.csv"
+        fasta_path = next(proj_folder.glob("*_reversed.fasta"))
+        yield get_coverages(*get_input_for_coverages(report_path, fasta_path))
+
+outlook = pd.DataFrame(iter_coverages(res))
 
 
+
+
+# compare this to the list of wanted results.
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.max_columns', 100)
+
+DIA = pd.read_csv(pt/'DIA.csv')
+
+
+with open(pt/'pool1.json'):
+
+list(res.glob("pool*/*/*/reversed_search/*_report.csv"))
 
