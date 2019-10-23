88d57959d4feed95c4abc0f98dec626fdf997f7d
MatteoLacki <matteo.lacki@gmail.com>
2019-10-23 12:53:26 +0200

    Counts are richtig

---
+++ proteome_tools_data/counts.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import pandas as pd
+
+RES = Path("D:\projects\proteome_tools\RES")
+res = list(RES.glob("pool*/[S,T]*/[S,T]*"))
+len(res)
+projects = {p.stem for p in res}
+DIA = pd.read_csv("J:/proteome_tools/DIA.csv").set_index('Raw_File')
+
+pd.set_option('display.max_rows', 100)
+DIA[~DIA.index.isin(projects)]
+
+raw_folders = Path("J:/proteome_tools/net")
+raw_folders = {r.stem:r for r in raw_folders.glob("*/WIRD_GESICHERT/[T,S]*/[T,S]*")}
